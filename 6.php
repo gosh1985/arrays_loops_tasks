b4f6a1fd78dd00e45f902095eda0ea67c2557454
Igor <ig1985@mail.ua>
2016-09-26 19:58:48 +0300

    first commit

---
+++ 6.php
@@ -0,0 +1,14 @@
+<?php
+
+$arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой');
+$en = [];
+$ru = [];
+foreach ($arr as $key=>$value){
+	$en []=$key;
+	$ru []=$value;
+	
+}
+print_r($en);
+echo '</br>';
+print_r($ru);
+?>
